5e37da2b62496b80e7880f0d07488b81faabb0fa
sistemastq <stqsistemas@gmail.com>
2025-12-30 10:49:48 -0500

    prueba 12

--- src/supabaseClient.ts
+++ src/supabaseClient.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 const SUPABASE_URL = process.env.SUPABASE_URL || "";
-const SUPABASE_KEY = process.env.SUPABASE_SERVICE_ROLE_KEY || "";
+const SUPABASE_KEY = process.env.SUPABASE_KEY || "";
 const SUPABASE_ANON_KEY = process.env.SUPABASE_ANON_KEY || "";
 
 if (!SUPABASE_URL) throw new Error("Falta SUPABASE_URL en el .env");
